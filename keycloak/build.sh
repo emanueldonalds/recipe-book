b33a3647727dd24c144bb76cfb732e5de1afa07d
Emanuel Donalds <emanueldonalds@gmail.com>
2022-04-25 19:24:44 +0300

    fix

--- keycloak/build.sh
+++ keycloak/build.sh
@@ -3,6 +3,6 @@
 if [[ ! -d "./rm -r keycloak-18.0.0.tar.gz" ]]
 then
     wget https://github.com/keycloak/keycloak/releases/download/18.0.0/keycloak-18.0.0.tar.gz
-    gunzip keycloak-18.0.0.tar.gz
+    tar -xvzf keycloak-18.0.0.tar.gz
     chmod +x keycloak-18.0.0/bin/kc.sh
 fi
