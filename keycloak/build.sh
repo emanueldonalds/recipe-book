fcdc79b6211b822c3ba2d96806098f2065cdfd1d
Emanuel Donalds <emanueldonalds@gmail.com>
2022-04-25 19:59:11 +0300

    Fix

--- keycloak/build.sh
+++ keycloak/build.sh
@@ -5,4 +5,6 @@ then
     wget https://github.com/keycloak/keycloak/releases/download/18.0.0/keycloak-18.0.0.tar.gz
     tar -xvzf keycloak-18.0.0.tar.gz
     chmod +x keycloak-18.0.0/bin/kc.sh
+else
+    echo "KeyCloak already downloaded"
 fi
