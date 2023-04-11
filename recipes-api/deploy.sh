62e0b1031446624b55dd91abaf9e9e80f7d9c3ea
Emanuel Donalds <emanueldonalds@gmail.com>
2023-04-11 19:32:31 +0300

    Add deploy script to api

--- recipes-api/deploy.sh
+++ recipes-api/deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+./gradlew clean
+./gradlew assemble
+./gradlew test
 ./gradlew bootJar
+
+echo Deploying jar file
+rm -r /home/recipes/deployments/recipes-api
+mkdir /home/recipes/deployments/recipes-api
+cp ./build/libs/recipes-api.jar /home/recipes/deployments/recipes-api/
+
+echo Restarting systemd service
+sudo systemctl daemon-reload
+sudo systemctl restart recipes-api.service
+
+echo Deployment done!
