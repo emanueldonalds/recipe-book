9dee117a529cc23ccdddf0e50de00e80690c1bbc
Emanuel Donalds <emanueldonalds@gmail.com>
2023-04-11 18:54:07 +0300

    Adjust service files to manjaro

--- recipes-frontend/src/app.js
+++ recipes-frontend/src/app.js
@@ -18,7 +18,7 @@ app.all('*', (req, res) => {
     res.status(404).send('<h1>404! Page not found</h1>');
   });
 
-app.listen(8080, function () {
-    console.log('Listening on port 8080!');
+app.listen(8090, function () {
+    console.log('Listening on port 8090!');
 });
 module.exports = app;
