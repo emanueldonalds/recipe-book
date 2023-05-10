5125b6128da180376607a040a3f7a54cb437dffa
Emanuel Donalds <emanueldonalds@gmail.com>
2023-05-10 16:49:30 +0300

    Remove unnecessary conf

--- recipes-api/src/main/java/recipes/recipes/api/configuration/JWTSecurityConfig.java
+++ recipes-api/src/main/java/recipes/recipes/api/configuration/JWTSecurityConfig.java
@@ -1,14 +1,9 @@
 package recipes.recipes.api.configuration;
 
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,11 +15,7 @@ import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -34,89 +25,17 @@ import java.util.stream.Stream;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class JWTSecurityConfig {
-    public interface Jwt2AuthoritiesConverter extends Converter<Jwt, Collection<? extends GrantedAuthority>> {
-    }
-
-    @SuppressWarnings("unchecked")
-    @Bean
-    public Jwt2AuthoritiesConverter authoritiesConverter() {
-        // This is a converter for roles as embedded in the JWT by a Keycloak server
-        // Roles are taken from both realm_access.roles & resource_access.{client}.roles
-        return jwt -> {
-            final var realmAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("realm_access", Map.of());
-            final var realmRoles = (Collection<String>) realmAccess.getOrDefault("roles", List.of());
-
-            final var resourceAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("resource_access", Map.of());
-
-            // We assume here you have "spring-addons-confidential" and "spring-addons-public" clients configured with "client roles" mapper in Keycloak
-            final var confidentialClientAccess = (Map<String, Object>) resourceAccess.getOrDefault("spring-addons-confidential", Map.of());
-            final var confidentialClientRoles = (Collection<String>) confidentialClientAccess.getOrDefault("roles", List.of());
-            final var publicClientAccess = (Map<String, Object>) resourceAccess.getOrDefault("spring-addons-public", Map.of());
-            final var publicClientRoles = (Collection<String>) publicClientAccess.getOrDefault("roles", List.of());
-
-            // Merge the 3 sources of roles and map it to spring-security authorities
-            return Stream.concat(
-                            realmRoles.stream(),
-                            Stream.concat(confidentialClientRoles.stream(), publicClientRoles.stream()))
-                    .map(SimpleGrantedAuthority::new).toList();
-        };
-    }
-
-    // spring-boot looks for a Converter<Jwt, ? extends AbstractAuthenticationToken> bean
-    // that is a converter from Jwt to something extending AbstractAuthenticationToken (and not AbstractAuthenticationToken itself)
-    // In this conf, we use JwtAuthenticationToken as AbstractAuthenticationToken implementation
-    public interface Jwt2AuthenticationConverter extends Converter<Jwt, JwtAuthenticationToken> {
-    }
-
-    @Bean
-    public Jwt2AuthenticationConverter authenticationConverter(Jwt2AuthoritiesConverter authoritiesConverter) {
-        return jwt -> new JwtAuthenticationToken(
-                jwt,
-                authoritiesConverter.convert(jwt),
-                jwt.getClaimAsString(StandardClaimNames.PREFERRED_USERNAME));
-    }
-
-    // Give lower precedence to security filter-chains without "securityMatcher"
-    // so that the filter-chains with a "securityMatcher" get a chance to be matched
-    // and this one is used as default for unmatched routes
-    @Order(Ordered.LOWEST_PRECEDENCE)
     @Bean
     public SecurityFilterChain apiFilterChain(
             HttpSecurity http,
-            ServerProperties serverProperties,
             Converter<Jwt, ? extends AbstractAuthenticationToken> authenticationConverter) throws Exception {
 
-        // Enable OAuth2 with custom authorities mapping
         http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(authenticationConverter);
-        // As the authentication bean is the one expected by spring-boot,
-        // an alternative would be to use just
-        // http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-
-        // Enable anonymous
-        http.anonymous();
-
-        // Enable and configure CORS
-        http.cors().configurationSource(corsConfigurationSource());
-
-        // State-less session (state in access-token only)
-        // with Disable CSRF because of disabled sessions
-        http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        // Return 401 (unauthorized) instead of 302 (redirect to login) when authorization is missing or invalid
-        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
-            response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Restricted Content\"");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
-        });
-
-        // If SSL enabled, disable http (https only)
-        if (serverProperties.getSsl() != null && serverProperties.getSsl().isEnabled()) {
-            http.requiresChannel().anyRequest().requiresSecure();
-        }
-
-        // Route security: authenticated to all routes but actuator and Swagger-UI
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                .requestMatchers(HttpMethod.HEAD, "/**").permitAll()
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/**").hasAuthority("editor")
                 .requestMatchers(HttpMethod.PUT, "/**").hasAuthority("editor")
                 .requestMatchers(HttpMethod.DELETE, "/**").hasAuthority("editor")
@@ -126,18 +45,36 @@ public class JWTSecurityConfig {
         return http.build();
     }
 
-    private CorsConfigurationSource corsConfigurationSource() {
-        // Very permissive CORS config...
-        final var configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setExposedHeaders(Arrays.asList("*"));
+    public interface Jwt2AuthoritiesConverter extends Converter<Jwt, Collection<? extends GrantedAuthority>> {
+    }
 
-        // Limited to API routes (neither actuator nor Swagger-UI)
-        final var source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+    @SuppressWarnings("unchecked")
+    @Bean
+    public Jwt2AuthoritiesConverter authoritiesConverter() {
+        return jwt -> {
+            final var realmAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("realm_access", Map.of());
+            final var realmRoles = (Collection<String>) realmAccess.getOrDefault("roles", List.of());
+            final var resourceAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("resource_access", Map.of());
+            final var confidentialClientAccess = (Map<String, Object>) resourceAccess.getOrDefault("spring-addons-confidential", Map.of());
+            final var confidentialClientRoles = (Collection<String>) confidentialClientAccess.getOrDefault("roles", List.of());
+            final var publicClientAccess = (Map<String, Object>) resourceAccess.getOrDefault("spring-addons-public", Map.of());
+            final var publicClientRoles = (Collection<String>) publicClientAccess.getOrDefault("roles", List.of());
 
-        return source;
+            return Stream.concat(
+                            realmRoles.stream(),
+                            Stream.concat(confidentialClientRoles.stream(), publicClientRoles.stream()))
+                    .map(SimpleGrantedAuthority::new).toList();
+        };
+    }
+
+    public interface Jwt2AuthenticationConverter extends Converter<Jwt, JwtAuthenticationToken> {
+    }
+
+    @Bean
+    public Jwt2AuthenticationConverter authenticationConverter(Jwt2AuthoritiesConverter authoritiesConverter) {
+        return jwt -> new JwtAuthenticationToken(
+                jwt,
+                authoritiesConverter.convert(jwt),
+                jwt.getClaimAsString(StandardClaimNames.PREFERRED_USERNAME));
     }
 }
